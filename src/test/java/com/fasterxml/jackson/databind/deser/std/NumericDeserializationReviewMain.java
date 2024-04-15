82c203bfa5e375ff0ffb11e6b9de6726c0878d1a
Dave Moten <davidmoten@gmail.com>
2024-04-15 11:12:15 +1000

    dev

--- src/test/java/com/fasterxml/jackson/databind/deser/std/NumericDeserializationReviewMain.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/std/NumericDeserializationReviewMain.java
@@ -34,30 +34,55 @@ public class NumericDeserializationReviewMain {
         List<Class<?>> classes = Arrays.asList(ByteProperty.class, ShortProperty.class, IntProperty.class,
                 LongProperty.class, FloatProperty.class, DoubleProperty.class, BigIntegerProperty.class,
                 BigDecimalProperty.class);
+        List<Class<?>> singleArgClasses = Arrays.asList(OfByte.class, OfShort.class, OfInt.class,
+                OfLong.class, OfFloat.class, OfDouble.class, OfBigInteger.class,
+                OfBigDecimal.class);
 
         for (int i = 0; i < values.size(); i++) {
             String value = values.get(i);
             String name = names.get(i);
             System.out.println();
-            System.out.println("------ " + name + "-----------");
+            System.out.println("------ Property " + name + "-----------");
             System.out.println("  value " + value);
             System.out.println();
             for (Class<?> cls : classes) {
-                String result;
-                String v;
-                try {
-                    v = new ObjectMapper().readValue(json(value), cls).toString();
-                    if (new BigDecimal(v).compareTo(new BigDecimal(value)) == 0) {
-                        result = "Ok";
-                    } else {
-                        result = "?";
-                    }
-                } catch (Throwable e) {
-                    v = value;
-                    result = "Err";
-                }
-                System.out.println(result + " " + cls.getSimpleName() + " " + v);
+                check(value, propertyJson(value), cls);
             }
+        }
+        
+        for (int i = 0; i < values.size(); i++) {
+            String value = values.get(i);
+            String name = names.get(i);
+            System.out.println();
+            System.out.println("------ SingleArg " + name + "-----------");
+            System.out.println("  value " + value);
+            System.out.println();
+            for (Class<?> cls : singleArgClasses) {
+                check(value, value, cls);
+            }
+        }
+
+    }
+
+    private static void check(String value, String json, Class<?> cls) {
+        String result;
+        String v;
+        String err = null;
+        try {
+            v = new ObjectMapper().readValue(json, cls).toString();
+            if (new BigDecimal(v).compareTo(new BigDecimal(value)) == 0) {
+                result = "Ok";
+            } else {
+                result = "?";
+            }
+        } catch (Throwable e) {
+            v = value;
+            result = "Err";
+            err = e.getMessage();
+        }
+        System.out.println(result + " " + cls.getSimpleName() + " " + v);
+        if (err != null) {
+//            System.out.println("  " + err);
         }
     }
 
@@ -68,8 +93,15 @@ public class NumericDeserializationReviewMain {
     // integer with decimal point (100.0) not parsed by any integer type (fail)
     // decimal types go to Infinity on overflow (acceptable)
     // decimal types lose precision rather than throw (good)
+    
+    // Conclusions for single-arg deserialization
+    //
+    // no support for byte, short, float
+    // integer in exponential form 1e2 not parsed by any integer type (fail)
+    // 1e2 not parsed by BigDecimal (fail)
+    // 9223372036854775807 (Long.MAX_VALUE) not parsed by BigDecimal (fail)
 
-    private static String json(String value) {
+    private static String propertyJson(String value) {
         return "{\"name\":\"saturn\",\"value\":\"" + value + "\"}";
     }
 
@@ -184,6 +216,110 @@ public class NumericDeserializationReviewMain {
 
         public BigDecimalProperty(@JsonProperty("name") String name, @JsonProperty("value") BigDecimal value) {
             this.name = name;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+    
+    public static final class OfByte {
+        final byte value;
+
+        public OfByte(byte value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+    
+    public static final class OfShort {
+        final short value;
+
+        public OfShort(short value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+    
+    public static final class OfInt {
+        final int value;
+
+        public OfInt(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+    
+    public static final class OfLong {
+        final long value;
+
+        public OfLong(long value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+    
+    public static final class OfFloat {
+        final float value;
+
+        public OfFloat(float value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+    
+    public static final class OfDouble {
+        final double value;
+
+        public OfDouble(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+    
+    public static final class OfBigInteger {
+        final BigInteger value;
+
+        public OfBigInteger(BigInteger value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+    
+    public static final class OfBigDecimal {
+        final BigDecimal value;
+
+        public OfBigDecimal(BigDecimal value) {
             this.value = value;
         }
 
